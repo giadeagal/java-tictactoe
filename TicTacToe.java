f1b4d41cb2e5caa78877e051449b3773019a502b
Gianluca <gl.galassini@gmail.com>
2022-01-05 23:03:05 +0100

    validation

--- TicTacToe.java
+++ TicTacToe.java
@@ -27,6 +27,7 @@ public static void main(String[] args) {
 
     printBoard(gameBoard);
     playerMove(gameBoard);
+    playerMove(gameBoard);
 
 }
 
@@ -110,7 +111,91 @@ public static void playerMove(char[][] gameBoard) {
 
     int move = input.nextInt();
 
+    boolean result = isValidMove(move, gameBoard);
+
+    while(!result) {
+        System.out.println("Casella non valida, provane un'altra");
+        move = input.nextInt();
+        result = isValidMove(move, gameBoard);
+    }
+
+    System.out.println("Il giocatore ha selezionato la casella " + move);
     updateBoard(move, 1, gameBoard);
 }
+
+public static boolean isValidMove(int move, char[][] gameBoard) {
+
+    switch (move) {
+
+        case 1:
+            if(gameBoard[0][0] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 2:
+            if(gameBoard[0][2] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 3:
+            if(gameBoard[0][4] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 4:
+            if(gameBoard[1][0] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 5:
+            if(gameBoard[1][2] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 6:
+            if(gameBoard[1][4] == '_') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 7:
+            if(gameBoard[2][0] == ' ') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 8:
+            if(gameBoard[2][2] == ' ') {
+                return true;
+            } else {
+                return false;
+            }
+
+        case 9:
+            if(gameBoard[2][4] == ' ') {
+                return true;
+            } else {
+                return false;
+            }
+        default:
+            return false;
+    }
+
+}
+
+
+
 
 }
